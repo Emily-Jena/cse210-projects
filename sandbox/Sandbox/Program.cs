55f22f0d5f19e34ee23597c3cd2593dbb629f1b1
Emily Jena <jen21062@byui.edu>
2025-04-29 10:51:33 -0600

    Added WriteLine Statement

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,5 +6,13 @@ class Program
     {
         Console.WriteLine("Hello Sandbox World!");
         Console.WriteLine("This is in C#");
+
+        int x = 10;
+        if (x==10)
+        {
+            Console.WriteLine("X is 10");
+        }
+
+        Console.WriteLine("Hey, how are you?");
     }
 }
