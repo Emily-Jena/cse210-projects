47694ec4ef9dae1ada17e10699a3dfe63d86a5d2
Emily Jena <jen21062@byui.edu>
2025-05-03 13:49:59 -0600

    guess number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Formats.Asn1;
+using System.Net;
+using System.Net.NetworkInformation;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Guess My number");
+        Console.WriteLine("----------------");
+        // ---------------Formula---------------------
+       
+        string repeat;
+        do 
+        {
+
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 101); 
+        int guessNum = -1;
+
+        
+        while (guessNum != magicNum)
+        {        
+        // Console.Write("What is the magic number? ");
+        // int magicNum = int.Parse(Console.ReadLine()); //ReadLine allows user input.
+        Console.Write("What is your guess? ");
+        guessNum =  int.Parse(Console.ReadLine());
+
+        // Formula to see if it is higher or lower.
+        
+            if (guessNum < magicNum )
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNum > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+            Console.Write("Do you want to continue? ");
+            repeat = Console.ReadLine();
+            } while (repeat == "yes");
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,18 +2,55 @@ using System;
 
 class Program
 {
+    static double AddNumbers(double a, double b) {
+        return a + b;
+    }
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
         Console.WriteLine("This is in C#");
 
-        int x = 10;
-        if (x==10)
-        {
-            Console.WriteLine("X is 10");
-        }
+        // int x = 10;
+        // if (x==10)
+        // {
+        //     Console.WriteLine("X is 10");
+        // }
 
-        Console.WriteLine("Hey, how are you?");
-        Console.WriteLine("Hey Betty, How are you?");
+        // Console.WriteLine("Hey, how are you?");
+        // Console.WriteLine("Hey Betty, How are you?");
+
+        // for (int i = 0; i < 20; i++);
+        // {
+        //     Console.WriteLine("Hello, Bob");
+        // }
+
+        // for (int i = 0; i < 20; i++)
+        // {
+        //     Console.WriteLine("Hello, Bob");
+
+        // }
+
+        // for (int i = 0; i < 20; i++)
+        // {
+        //     Console.WriteLine($"Hello, Bob {i}");
+
+        // }
+
+        // List<int> myNumbers = new List<int>();
+        // myNumbers.Add(99);
+        // myNumbers.Add(25);
+        // myNumbers.Add(-132);
+        // //myNumbers.Append(21);
+        // myNumbers.Add(230);
+
+        // foreach(int i in myNumbers)
+        // {
+        //     Console.WriteLine(i);
+        //}
+
+    double total = AddNumbers(123.98, 985.12356);
+    Console.WriteLine(total); //calling the return code
+    
+
     }
 }
