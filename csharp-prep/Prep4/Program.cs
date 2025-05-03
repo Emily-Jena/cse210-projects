d2fdef849b18327f8d1f5420078fcb1f1e75599e
Emily Jena <jen21062@byui.edu>
2025-05-03 17:47:04 -0600

    Appended numbers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,57 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Appended List");
+        Console.WriteLine("------------------");
+        // ----------------------------------------------
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+        int inputNum = -1;
+
+        while (inputNum != 0)
+        {   
+        Console.Write("Enter number: ");
+        inputNum = int.Parse(Console.ReadLine());
+
+        if (inputNum != 0)
+        {
+            numbers.Add(inputNum);
+        }
+        }
+
+        if (numbers.Count > 0) 
+        {
+        // ----Calculate Sum ----------
+        int sumNum = 0;
+        foreach (int number in numbers) 
+        {
+            sumNum += number;
+        }
+        Console.WriteLine($"The sum is {sumNum}");
+
+        // ------Calculate Average -------------
+        float aveNum = ((float)sumNum) / numbers.Count;
+        Console.WriteLine($"The average is {aveNum}");
+
+        // ------ Calculate Max Digit ---------------
+        int largeNum = numbers[0];
+        foreach (int number in numbers)
+        {        
+            if (largeNum < number) 
+            {
+                largeNum = number;
+            }
+        }
+        Console.WriteLine($"The largest number is {largeNum}");
+        } 
+
     }
-}
+} 
